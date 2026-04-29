38766ad76dc4acd3540fd71af0c06c54b9583df5
tingamapuro04 <alponcemobutu100@gmail.com>
2026-04-29 08:25:25 +0300

    Add authorization based on roles

--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,9 @@ import { handleMpesaCallback } from "../mpesa/callback.js";
 const orderRouter = Router();
 
 import { createOrder, getOrdersByUserId } from "../controllers/orders.js";
-orderRouter.post("/", createOrder);
+import { authenticateToken } from "../middlewares/authenticateToken.js";
+import { authorizeRoles } from "../middlewares/authorize.js";
+orderRouter.post("/", authenticateToken, authorizeRoles('user'), createOrder);
 orderRouter.post("/mpesa/callback", handleMpesaCallback);
 orderRouter.get("/user/:userId", getOrdersByUserId);
 
--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,11 @@ const userRouter = Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-import { getAllUsers, getUserById, registerUser } from "../controllers/user.js";
+import { getAllUsers, getUserById, registerUser, loginUser } from "../controllers/user.js";
 
 userRouter.get("/", getAllUsers);
 userRouter.get("/:id", getUserById);
 userRouter.post("/", upload.single("profilePicture"), registerUser);
+userRouter.post("/login", loginUser);
+
 export default userRouter;
